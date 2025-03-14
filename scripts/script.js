7f40eed598bfdcf930d6fc2bf9347cbdbfa3aed4
biplab-mistry <kr.biplabmistry@gmail.com>
2025-03-14 11:05:40 +0530

    search integrated

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ const loadCategory = async () => {
 const showCategory = (category) => {
 
     category.forEach(cate => {
-    
+
         const container = document.getElementById("category-container")
         const div = document.createElement("div")
         div.innerHTML = `
@@ -29,33 +29,42 @@ async function categoryVideo(id) {
     const btn = document.getElementById(`btn-${id}`);
 
 
-    const allButtons=document.querySelectorAll("#category-container button")
+    const allButtons = document.querySelectorAll("#category-container button")
     allButtons.forEach(btn => btn.classList.remove("active"));
-    
+
 
     btn.classList.add("active")
 
-  
-   
+
+
 
 }
 
 
 // get video data
-async function loadVideo() {
-    const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+async function loadVideo(value="") { 
+    const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${value}`)
     const data = await res.json()
     const btn0 = document.getElementById("btn-0");
-    const allButtons=document.querySelectorAll("#category-container button")
+    const allButtons = document.querySelectorAll("#category-container button")
     allButtons.forEach(btn => btn.classList.remove("active"));
-    
+
     btn0.classList.add("active")
 
     showVideo(data.videos);
 
 }
+// search video
+document.getElementById('searching').addEventListener("keyup",function(e){
+    const value=e.target.value;
+   loadVideo(value)
+    
+
+})
 //  show video
 function showVideo(videos) {
+   
+
     if (videos.length == 0) {
         document.getElementById("video-container").innerHTML = "";
         document.getElementById("no-video").innerHTML = ""
@@ -70,21 +79,45 @@ function showVideo(videos) {
         document.getElementById("no-video").innerHTML = ""
         videos.forEach(video => {
             const div = document.createElement("div")
+      
+
             div.innerHTML = `
+
               <div class="flex flex-col">
                         <div >
                             <img class="object-cover h-[200px] w-[320px] rounded-md" src="${video.thumbnail}" alt="">
                         </div>
-                        <div class="flex gap-3 items-center pt-4">
+                        <div class="flex gap-3 items-center pt-4 justify-between">
                             <div class="avatar w-5">
                                 <div class="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
                                   <img src="${video.authors[0].profile_picture}" />
                                 </div>
                               </div>
                             <div>  <h3>${video.title}</h3>
+                              <div class="flex gap-2">
                               <p>${video.authors[0].profile_name}</p> 
+
+                         ${video.authors[0].verified ? `<img class="w-6" src="social-media.png" alt="social">` : ""}
+
+                              
+
+                              </div>
+        
                               <p>${video.others.views} Views</p> 
                               </div>
+                                <label for="my_modal_6" class="btn">Details</label>
+                                 <input type="checkbox" id="my_modal_6" class="modal-toggle" />
+        <div class="modal" role="dialog">
+            <div class="modal-box">
+                <h3 class="text-lg font-bold">Hello!</h3>
+                <p class="py-4">${video.title}</p>
+                <p class="py-4">${video.authors[0].profile_name}</p>
+                <p class="py-4">${video.description}</p>
+                <div class="modal-action">
+                    <label for="my_modal_6" class="btn">Close!</label>
+                </div>
+            </div>
+        </div>
                         </div>
                       
                     </div>      
